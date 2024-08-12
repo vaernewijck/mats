fe8695acfa884f01a371bc720793f9935903f9d9
vaernewijck <pieter-jan.vaernewijck@outlook.be>
2024-08-12 18:13:37 +0200

    fix timer, dad brain

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+// TODO: fix redirect bug
+
 const countElement = document.querySelector('.count');
-let counter = 4;
+let counter = 5;
 
 const countdownInterval = setInterval(() => {
   counter--;
+
   if (counter > 0) {
     countElement.textContent = `${counter} ${counter > 1 ? ' seconden' : ' seconde'}!`;
   } else {
     window.location.href = 'https://pakske.be/lijst/mats-vaernewijck';
+    countElement.textContent = `Let's go!`;
+
     clearInterval(countdownInterval);
   }
 }, 1000);
