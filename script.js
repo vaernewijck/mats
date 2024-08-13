60e256350501ebfc89b5089cbc56f27360b66947
vaernewijck <pieter-jan.vaernewijck@outlook.be>
2024-08-13 11:50:40 +0200

    forgot redirect, dad brain. need sleep

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const countdownInterval = setInterval(() => {
     const coundown = document.querySelector('.countdown');
     coundown.innerHTML = '<a href="https://pakske.be/lijst/mats-vaernewijck">geboortelijst</a>';
 
-    //window.location.assign('https://pakske.be/lijst/mats-vaernewijck');
+    window.location.assign('https://pakske.be/lijst/mats-vaernewijck');
     clearInterval(countdownInterval);
   }
 }, 1000);
